d3476418a21d8d10b6e3ee9d50d35622ade8e016
Lucas Gulen <lucas.gulen@hotmail.ch>
2020-04-02 15:34:19 +0200

    Start login, delete home page and modify routing to login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  userTelephone: string;
+  userUsername: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  searchTelephoneNumber() {
+    const telephone =  localStorage.getItem('telephone');
+    console.log(telephone);
+  }
+
+  saveTelephoneNumber() {
+    localStorage.setItem('telephone', this.userTelephone);
+    localStorage.setItem('username', this.userUsername)
+    console.log(this.userTelephone);
+    console.log(this.userTelephone);
   }
 
 }
