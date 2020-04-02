819d7993be0936c83233ade8df7ca0dd6e7bd3a6
BarreiroLindoF <flavio_slb4@hotmail.com>
2020-04-03 01:56:09 +0200

    Derniere correction de merge

--- src/app/main-livreur/main-livreur.page.ts
+++ src/app/main-livreur/main-livreur.page.ts
@@ -1,12 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Aid} from '../interfaces/Aid';
-import {Status} from '../interfaces/Status';
+import { Component, OnInit } from '@angular/core';
+import { Aid } from '../interfaces/Aid';
+import { Status } from '../interfaces/Status';
 import { AidType } from '../interfaces/AidType';
-<<<<<<< HEAD
 import { GeoPosition } from '../interfaces/GeoPosition';
-=======
-import {GeoPosition} from '../interfaces/GeoPosition';
->>>>>>> f700bb62a3c25565e8f64f15c447021555cc15af
 
 @Component({
     selector: 'app-main-livreur',
@@ -24,8 +20,8 @@ export class MainLivreurPage implements OnInit {
         const aid: Aid = {
             id: 1,
             text: 'Salut c\'est moi',
-            seniorUser: {username: 'Flavieaux', phone: '121843960'},
-            aidUser: {username: 'Loïque', phone: '121843960'},
+            seniorUser: { username: 'Flavieaux', phone: '121843960' },
+            aidUser: { username: 'Loïque', phone: '121843960' },
             location: new GeoPosition(46, 6),
             status: Status.CREATED,
             aidType: AidType.SHOPPING,
